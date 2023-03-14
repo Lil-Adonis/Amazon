81dfb7b8281ec2d65bde047bc13a259c704143af
Lil-Adonis <adenugaib24@gmail.com>
2023-03-14 13:03:10 -0700

    :construction: Fakestore Api configuration
    Fakestore Api serverside Remdering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import  Banner from '../components/Banner'
+import ProductFeed from '../components/ProductFeed'
 import Header from '../components/Header'
 
 const Home: NextPage = () => {
@@ -12,8 +14,27 @@ const Home: NextPage = () => {
       </Head>
       {/* Header */}
       <Header/>
+
+      <main className="max-w-screen-2xl mx-auto">
+        <Banner />
+        <ProductFeed products={products} />
+      </main>
     </div>
   )
 }
 
 export default Home
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  const products = await fetch(
+    "https://hasinduperera12.github.io/amazonAPi/amazon.json"
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      products: products,
+      session: session,
+    },
+  };
+}
