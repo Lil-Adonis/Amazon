110e55a46f7ca6f96356e0e9ba0c64d689465260
Lil-Adonis <adenugaib24@gmail.com>
2023-03-14 15:35:23 -0700

    :package: installed, added and configured firebase and NextAuth
    firebase && Next Auth

---
+++ firebase/firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA5qetWcM_vTdz3arYWZEyGje1B-WVDAw8",
+    authDomain: "build-5ccc7.firebaseapp.com",
+    projectId: "build-5ccc7",
+    storageBucket: "build-5ccc7.appspot.com",
+    messagingSenderId: "583067098498",
+    appId: "1:583067098498:web:b2b598bc042856cc0d839b"
+  };
