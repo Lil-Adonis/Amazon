99ff3d75d60af735874ae79c98a8c7bbb4861364
Lil-Adonis <adenugaib24@gmail.com>
2023-03-13 18:28:48 -0700

    :art: Header
    Header component styled

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
-}
+  images: {
+    domains: ["fakestoreapi.com", "drive.google.com"],
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    extend: {
+      colors: {
+        amazon_blue: {
+          light: "#232F3E",
+          DEFAULT: "#131921",
+        },
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
-}
+};
