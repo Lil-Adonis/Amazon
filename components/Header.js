3e2935a99e53ab24f33fef4b55ba3d1848a4d3ca
Lil-Adonis <adenugaib24@gmail.com>
2023-03-15 14:21:32 -0700

    :hammer: Writing code for Redux and Checkout component
    Redux,Redux toolkit && CheckOut component

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,21 @@
 import Image from "next/image";
 import { signIn, signOut, useSession } from "next-auth/react";
 import React from "react";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { selectItems } from "../slices/basketSlice";
 import { Search, ShoppingCart, MenuIcon } from "../components/icons/index";
 
 const Header = () => {
+  const { data: session } = useSession();
+  const router = useRouter();
+  const items = useSelector(selectItems);
 
   return (
     <header>
       <div className="flex items-center bg-amazon_blue p-1 flex-grow">
         <div
-          
+          onClick={() => router.push("/")}
           className="mt-2 flex items-center flex-grow sm:flex-grow-0"
         >
           <Image
@@ -29,22 +35,22 @@ const Header = () => {
         </div>
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
           <div
-            onClick={signIn}
+            onClick={() => (session ? signOut() : signIn())}
             className="link"
           >
-            <p>Hello Lil-Adonis</p>
+            <p>{session ? `Hello ${session?.user?.name}` : "Sign In"}</p>
             <p className="font-extrabold md:text-sm">Account & List</p>
           </div>
-          <div className="link">
+          <div onClick={() => router.push("/orders")} className="link">
             <p>Return</p>
             <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
           <div
-            
+            onClick={() => router.push("/checkout")}
             className="link relative flex items-center"
           >
             <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full font-bold">
-              
+              {items.length}
             </span>
             <ShoppingCart className="h-10 text-white" />
             <p className="hidden md:inline mt-2 font-extrabold md:text-sm">
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,15 @@
 import "../styles/globals.css";
-import { SessionProvider } from "next-auth/react";
-import "../styles/globals.css";
 import { Provider } from "react-redux";
+import { store } from "../atom/store";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
-      <Provider>
+      <Provider store={store}>
         <Component {...pageProps} />
       </Provider>
     </SessionProvider>
   );
 }
+
